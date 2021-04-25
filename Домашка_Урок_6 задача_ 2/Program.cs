c16fc86f8cb67dfa703b2d5ca4bec4a885b99942
Александр Шаповаленко <san19922@yandex.ru>
2021-04-25 17:29:01 +0300

    Задание 6.2 добавление делегата

--- Домашка_Урок_6 задача_ 2/Program.cs
+++ Домашка_Урок_6 задача_ 2/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Домашка_Урок_6_задача__2
+namespace HomeworkL6._2
 {
+    //Александр Шаповаленко
+    //2. Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+    //а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум.
+    //   Использовать массив(или список) делегатов, в котором хранятся различные функции.
+    //б) *Переделать функцию Load, чтобы она возвращала массив считанных значений.Пусть она возвращает минимум через параметр (с использованием модификатора out).
+
+    public delegate double Del(double x);
     class Program
     {
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        public static void SaveFunc(string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h; 
+                Console.WriteLine(x);
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+                Console.WriteLine(min);
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
         static void Main(string[] args)
         {
+            SaveFunc("data.bin", -2, 2, 1);
+            Console.WriteLine(Load("data.bin"));
+            Console.ReadKey();
         }
     }
+
 }
