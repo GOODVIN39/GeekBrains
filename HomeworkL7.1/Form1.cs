7c7cb73519d940a5ff1151effb58eedf9dff53d7
Александр Шаповаленко <san19922@yandex.ru>
2021-05-02 15:42:12 +0300

    Дз 7

---
+++ HomeworkL7.1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace HomeworkL7._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (int.Parse(label1.Text) + 1).ToString();
+            label3.Text = (int.Parse(label3.Text) + 1).ToString();
+            if (label1.Text == label6.Text)
+            {
+                MessageBox.Show("Поздравляю вы победили!");
+            }
+        }
+        public void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = (int.Parse(label1.Text) * 2).ToString();
+            label3.Text = (int.Parse(label3.Text) + 1).ToString();
+            if (label1.Text == label6.Text)
+            {
+                MessageBox.Show("Поздравляю вы победили!");
+                this.Close();
+            }
+        }
+        public void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            label3.Text = (int.Parse(label3.Text) + 1).ToString();
+            
+        }
+        public void button4_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            Random r = new Random();
+            int count = r.Next(200);
+            MessageBox.Show($"Получите число: {count}");
+            button4.Visible = false;
+            label6.Text = Convert.ToString(count);            
+        }
+
+        
+    }
+}
---
+++ HomeworkL7.2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeworkL7._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int Random()
+        {
+            Random rand = new Random();
+            int count = rand.Next(1, 10);
+            return count;
+        }
+        public int Value()
+        {
+            int b;
+            var a = int.TryParse(textBox1.Text, out b);
+            return b;
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (Random() == Value())
+            {
+                MessageBox.Show("Вы угадали");
+                this.Close();
+            }            
+            else if (Value() > 10) MessageBox.Show("Введите число меньше или равно 10");            
+            else if (Value() < 1) MessageBox.Show("Введите число больше или равно 1");      
+        }        
+    }
+}
